04afcc5e57e6bcbb525c71bbbf674276e9d7b79b
JargoB <botcherjargo@gmail.com>
2026-09-10 11:41:29 +0300

    nesting2

---
+++ IfAndElseNesting2/IfAndElseNesting2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IfAndElseNesting2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bleh");
+
+            int number = 67;
+
+            if (number > 67)
+            {
+                Console.WriteLine("Number on 67");
+            }
+            else if (number > 77)
+            {
+                if(number == 82)
+                {
+                    Console.WriteLine("Number võrdub 82");
+                }
+                else
+                {
+                    Console.WriteLine("Number on teises bahemikus");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vastus oli 67");
+            }
+        }
+    }
+}
