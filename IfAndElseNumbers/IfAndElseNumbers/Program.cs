b42b1bd71a8c3249df7bd3f4bbf1fc14aac4aa99
JargoB <botcherjargo@gmail.com>
2026-09-10 10:51:42 +0300

    IfAndElseNumbers

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Linq;
+
+namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            //kasutada if ja else lauseid
+
+            Console.WriteLine("Sisesta number:");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine($"Sisestasid numbri : {number}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR.Numbrit ei sisestanud");
+
+                Console.Beep();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
